a6faafd9e30c7da007b1ca1c38a2faa9ce1aa29e
Shriyut Jha <46967628+Shriyut@users.noreply.github.com>
2021-04-15 20:32:39 +0530

    Create cutall.sh

---
+++ shell-scripting-master/cutall.sh
@@ -0,0 +1,31 @@
+#https://www.hackerrank.com/challenges/text-processing-cut-2/problem
+
+cut -b 2,7
+
+#https://www.hackerrank.com/challenges/text-processing-cut-3/problem
+
+cut -b 2-7
+
+#https://www.hackerrank.com/challenges/text-processing-cut-4/problem
+
+cut -b -4
+
+#https://www.hackerrank.com/challenges/text-processing-cut-5/problem
+
+cut -d $'\t' -f1-3
+
+#https://www.hackerrank.com/challenges/text-processing-cut-6/problem
+
+cut -c 13-
+
+#https://www.hackerrank.com/challenges/text-processing-cut-7/problem
+
+cut -d " " -f 4
+
+#https://www.hackerrank.com/challenges/text-processing-cut-8/problem
+
+cut -d " " -f 1-3 
+
+#https://www.hackerrank.com/challenges/text-processing-cut-9/problem
+
+cut -d $'\t' -f 2-
